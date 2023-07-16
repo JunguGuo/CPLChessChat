70a1568bf041505f77a5f7ff08cdddc1f35b317f
Jungu Guo <hey.jungu@gmail.com>
2023-07-16 02:56:57 -0400

    update

--- components/sidebar.py
+++ components/sidebar.py
@@ -9,25 +9,11 @@ import os
 
 def sidebar():
     with st.sidebar:
-        st.markdown(
-            "## How to use\n"
-            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
-            "2. Upload a pdf, docx, or txt file📄\n"
-            "3. Ask a question about the document💬\n"
-        )
-
-        st.markdown("---")
+        
         st.markdown("# About")
         st.markdown(
-            "📖KnowledgeGPT allows you to ask questions about your "
-            "documents and get accurate answers with instant citations. "
+            "Chess GPT allows you to ask questions about the John G. White "
+            "chess collection in Cleveland Public Library "
         )
-        st.markdown(
-            "This tool is a work in progress. "
-            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
-            "with your feedback and suggestions💡"
-        )
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
-        st.markdown("---")
 
         faq()
