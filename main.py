177c7bcca346e4f8ecd82bcd24accd06bc871b13
Jungu Guo <hey.jungu@gmail.com>
2023-07-16 02:52:15 -0400

    update

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            .css-1a1tcp.e1ewe7hr3 {display: none;}
+            .stActionButton .css-1a1tcp.e1ewe7hr3 {display: none;}
             </style>
             """
-#st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 sidebar()
 
