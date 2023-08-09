ee7fd709a7e89770df88ce4901a2d40e77c685a6
Jungu Guo <hey.jungu@gmail.com>
2023-08-09 02:40:09 -0400

    update

--- components/faq.py
+++ components/faq.py
@@ -6,11 +6,13 @@ def faq():
     st.markdown(
         """
 # FAQ
-## How does Chess Chat work?
-TBD
+## About this project?
+The integration of AI in the Cleveland Public Library's endeavors offers an inspiring outlook on the future of library services. This project is part of a roadmap to achieving a symbiotic integration of AI and human librarians, to inspire and engage users in CPL’s rich chess collection.
 
 
-## Are the answers 100% accurate?
-TBD
+## About the special collections in Cleveland Public Library?
+Building on this firm foundation, the Cleveland Public Library consolidated rare holdings from the subject departments into a unified collection designed for optimum housing, preservation and security. Special Collections continues to benefit from the generosity of donors in acquiring individual items as well as entire collections. Researchers now and in the future are assured availability of the Library’s greatest treasures. While most of the special collections are listed in the Library’s online catalog, some materials are listed only in the card files housed in the Fine Arts and Special Collections reading room.
+
+The core of the collection focuses on materials related to chess and checkers, folklore, and the peoples and histories of Africa, Oceania and Asia.
 """
     )
